a792e13898dcf31ffe11743d1dc73788effd742f
Menne Bos <mennebos@gmail.com>
2025-03-22 01:15:13 +0100

    Enhance PlayLight skill with room entity support and improve dialog localization

--- __init__.py
+++ __init__.py
@@ -10,13 +10,19 @@ class LightSkill(OVOSSkill):
     def initialize(self):
         self.add_event('mycroft.light.play', self.handle_play_light)
         self.register_intent_file('PlayLight.intent', self.handle_play_light)
+        self.register_entity_file('room.entity')
 
     def handle_play_light(self, message: Message):
+        room_type = message.data.get('room')
+        if room_type is not None:
+            self.speak_dialog('PlayLight',
+                              {'room': room_type})
+        else:
+            self.speak_dialog('PlayLight')
         #url = f"http://192.168.1.45/api/manager/logic/webhook/Terre/?tag=Light"
-        url = f"http://192.168.1.187/api/manager/logic/webhook/Demo/?tag=Light"
+        url = f"http://192.168.1.187/api/manager/logic/webhook/Demo/?tag="+room_type
         data = requests.get(url)
         print(data.json())
-        self.speak_dialog("PlayLight")
         # self.play_audio("/home/ovos/.venvs/ovos/lib/python3.11/site-packages/skill_ovos_melody/soundbytes/As_You_Wish.mp3", False) 
 
 def create_skill():
