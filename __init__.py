26017c92a0199f37fc94f10de3e7f76d11e04c6f
Menne Bos <mennebos@gmail.com>
2025-03-26 20:32:47 +0100

    Refactor LightSkill to improve initialization and add runtime requirements; remove unused intents and entities, and update dialog files for better functionality.

--- __init__.py
+++ __init__.py
@@ -1,66 +1,120 @@
-from ovos_workshop.skills.ovos import OVOSSkill
+from ovos_utils import classproperty
+from ovos_utils.log import LOG
+from ovos_workshop.intents import IntentBuilder
+from ovos_utils.process_utils import RuntimeRequirements
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
 from ovos_bus_client.message import Message
-import os
 import requests
 
+DEFAULT_SETTINGS = {
+    "log_level": "INFO"
+}
+
 class LightSkill(OVOSSkill):
-    def __init__(self):
-        super().__init__("LightSkill")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.override = True
+    
+    @classproperty
+    def runtime_requirements(self):
+        # if this isn't defined the skill will
+        # only load if there is internet
+        return RuntimeRequirements(
+            internet_before_load=False,
+            network_before_load=True,
+            gui_before_load=False,
+            requires_internet=False,
+            requires_network=True,
+            requires_gui=False,
+            no_internet_fallback=True,
+            no_network_fallback=True,
+            no_gui_fallback=True,
+        )
 
     def initialize(self):
-        self.add_event('mycroft.light.play', self.handle_play_light)
-        self.register_intent_file('PlayLight.intent', self.handle_play_light)
+        self.settings.merge(DEFAULT_SETTINGS, new_only=True)
+        self.settings_change_callback = self.on_settings_changed
+        self.add_event('mycroft.room.play', self.handle_room_light)
+        self.add_event('mycroft.all.play', self.handle_all_light)
+        self.add_event('mycroft.toggle.play', self.handle_toggle_light)
         self.register_entity_file('room.entity')
         self.register_entity_file('action.entity')
+        self.register_entity_file('device.entity')
+        self.register_entity_file('expression.entity')
+        self.register_entity_file('lid.entity')
+    
+    def on_settings_changed(self):
+        """This method is called when the skill settings are changed."""
+        LOG.info("Settings changed!")
 
-    def handle_play_light(self, message: Message):
+    @property
+    def log_level(self):
+        """Dynamically get the 'log_level' value from the skill settings file.
+        If it doesn't exist, return the default value.
+        This will reflect live changes to settings.json files (local or from backend)
+        """
+        return self.settings.get("log_level", "INFO")
+
+    @intent_handler("RoomLight.intent")
+    def handle_room_light(self, message: Message):
         room_type = message.data.get('room')
         action_type = message.data.get('action')
         device_type = message.data.get('device')
         expression_type = "gedaan"
         
-        if action_type is "give":
-            action_type = "on"
-        if action_type is "dark":
-            action_type = "off"
-        if room_type is "hang":
-            room_type = "main"
-        if room_type is "central":
-            room_type = "main"
-        if room_type is "led":
-            room_type = "strip"
+        if device_type in ("lamp", "lampen", "lichten", "verlichting"):
+            lid_type = "de"
+        if device_type in ("licht"):
+            lid_type = "het"
+
+        self.speak_dialog('LightOffOn',
+                {'lid': lid_type, 'room': room_type, 'device': device_type, 'action': action_type, 'expression':expression_type})
+        url = f"http://192.168.1.187/api/manager/logic/webhook/Demo/?tag=Light"+room_type+action_type
+        data = requests.get(url)
+        print(data.json())
+
+    @intent_handler("AllLight.intent")            
+    def handle_all_light(self, message: Message):
+        action_type = message.data.get('action')
+        device_type = message.data.get('device')
+        expression_type = "gedaan"
+        room_type = "alle"
+        
+        if device_type in ("licht", "verlichting"):
+            device_type = "lichten"
+            lid_type = "de"
+        if device_type in ("lamp"):
+            device_type = "lampen"
+            lid_type = "de"
+        if device_type in ("licht"):
+            lid_type = "het"
+        
+        self.speak_dialog('LightOffOn',
+                {'lid': lid_type, 'room': room_type, 'device': device_type, 'action': action_type, 'expression':expression_type})
+        url = f"http://192.168.1.187/api/manager/logic/webhook/Demo/?tag=Light"+room_type+action_type
+        data = requests.get(url)
+        print(data.json())
+
+    @intent_handler("ToggleLight.intent")      
+    def handle_toggle_light(self, message: Message):
+        room_type = message.data.get('room')
+        device_type = message.data.get('device')
+        action_type = " "
+        expression_type = "aangepast"
 
         if device_type in ("lamp", "lampen", "lichten", "verlichting"):
             lid_type = "de"
         if device_type in ("licht"):
             lid_type = "het"
 
-        if room_type is " ":  # if no room is given apply to all lights
-            if device_type in ("lampen", "lichten"):	
-                room_type = "alle"
-                lid_type = " "
-            elif device_type in ("licht", "verlichting"):
-                room_type = "alle"
-                device_type = "lichten"
-                lid_type = " "
-            elif device_type in ("lamp"):
-                room_type = "alle"
-                device_type = "lampen"
-                lid_type = " "
- 
-
-        if action_type is " ":  # if no action is given apply toggle flow
-            action_type = "aangepast"
-            expression_type = " "
-
         self.speak_dialog('LightOffOn',
-                            {'lid': lid_type, 'room': room_type, 'device': device_type, 'action': action_type, 'expression':expression_type})
-        
-        #url = f"http://192.168.1.45/api/manager/logic/webhook/Terre/?tag=Light"
+                {'lid': lid_type, 'room': room_type, 'device': device_type, 'action': action_type, 'expression':expression_type})
         url = f"http://192.168.1.187/api/manager/logic/webhook/Demo/?tag=Light"+room_type+action_type
         data = requests.get(url)
         print(data.json())
-        # self.play_audio("/home/ovos/.venvs/ovos/lib/python3.11/site-packages/skill_ovos_melody/soundbytes/As_You_Wish.mp3", False) 
+
+    #url = f"http://192.168.1.45/api/manager/logic/webhook/Terre/?tag=Light"
 
 def create_skill():
     return LightSkill()
